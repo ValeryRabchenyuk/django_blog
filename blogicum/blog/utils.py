b1f131d756e3e458bd3433599aafe8a8f0ff0c85
Valery Rabchenyuk <valery0623@gmail.com>
2024-09-17 18:53:52 +0300

    'paginator' added to utils.py

--- blogicum/blog/utils.py
+++ blogicum/blog/utils.py
@@ -1,4 +1,18 @@
+from django.utils import timezone
+
+from django.core.paginator import Paginator
+
+from .constants import PAGINATOR_NUMBER
+
+
 def post_filter(queryset):
     return queryset.filter(is_published=True,
                            category__is_published=True,
                            pub_date__lte=timezone.now())
+
+
+def post_paginator(request, queryset,
+                   number_of_pages=PAGINATOR_NUMBER):
+    paginator = Paginator(queryset, number_of_pages)
+    page_number = request.GET.get('page')
+    return paginator.get_page(page_number)
