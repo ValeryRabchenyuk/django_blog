ff399b358fd40a51bf85ef29e0efae4da74f5307
Valery Rabchenyuk <valery0623@gmail.com>
2024-09-17 13:36:08 +0300

    403 CSRF, 404, 500 error pages

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,12 +1,35 @@
-"""Импорт функции."""
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
-def about(request):
-    """Рендеринг страницы о проекте."""
-    return render(request, 'pages/about.html')
+class AboutPage(TemplateView):
+    template_name = 'pages/about.html'
+
+# def about(request):
+#     """Рендеринг страницы о проекте."""
+#     return render(request, 'pages/about.html')
 
 
-def rules(request):
-    """Рендеринг страницы с правилами."""
-    return render(request, 'pages/rules.html')
+class RulesPage(TemplateView):
+    template_name = 'pages/rules.html'
+
+
+# def rules(request):
+#     """Рендеринг страницы с правилами."""
+#     return render(request, 'pages/rules.html')
+
+
+def permission_denied(request, exception):
+    return render(request, "pages/403.html", status=403)
+
+
+def csrf_failure(request, reason=''):
+    return render(request, 'pages/403csrf.html', status=403)
+
+
+def page_not_found(request, exception):
+    return render(request, 'pages/404.html', status=404)
+
+
+def server_error(request):
+    return render(request, "pages/500.html", status=500)
